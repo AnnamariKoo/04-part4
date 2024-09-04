56a75b06059b021b464cd68833c407ecd2c3a55f
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2024-09-04 13:00:00 +0300

    Tapahtumankäsittely tehtäväkansio luotu.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = (props) => {
+  const [value, setValue] = useState(10);
+
+  return (
+    <div>
+      {value}
+      <button>reset to zero</button>
+    </div>
+  );
+};
+export default App;
