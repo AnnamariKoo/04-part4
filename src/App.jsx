6824ad57eff08de07ca38bbe9223c10a04e2a2b6
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2024-09-11 12:28:07 +0300

    Tehtävä tehty valmiiksi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
-const App = (props) => {
+const Display = (props) => <div> {props.value} </div>;
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
+const App = () => {
   const [value, setValue] = useState(10);
 
   const setToValue = (newValue) => {
@@ -8,13 +14,9 @@ const App = (props) => {
     setValue(newValue);
   };
 
-  const Button = (props) => (
-    <button onClick={props.handleClick}>{props.text}</button>
-  );
-
   return (
     <div>
-      {value}
+      <Display value={value} />
       <Button handleClick={() => setToValue(1000)} text="thousand" />
       <Button handleClick={() => setToValue(0)} text="reset" />
       <Button handleClick={() => setToValue(value + 1)} text="increment" />
