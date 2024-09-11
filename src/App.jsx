c8da949039c91bfb51a34b8a30f0979eda4f3f41
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2024-09-11 11:53:59 +0300

    Tapahtumankäsittelijäfunktiossa kaksi komentoa: console.log ja setValue.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 const App = (props) => {
   const [value, setValue] = useState(10);
+  const handleClick = () => {
+    console.log("clicked the button");
+    setValue(0);
+  };
 
   return (
     <div>
       {value}
-      <button>reset to zero</button>
+      <button onClick={handleClick}>button</button>
     </div>
   );
 };
+
 export default App;
